42008a613f93cb37b469b6958ba500344d356557
Quentin Vandevorst <56817496+Survive95@users.noreply.github.com>
2020-09-15 15:35:15 +0200

    init piano

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import './App.scss';
+import * as Tone from 'tone'
 
 function App() {
+
+  const synth = new Tone.Synth().toDestination()
+
+  const note = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  // note.reverse()
+  const notes = []
+
+
+  function loop() {
+    for (let i = 0; i < 11; i++) {
+      note.map(item => {
+        notes.push(item + i)
+      })
+    }
+    notes.reverse()
+  }
+
+  loop()
+
+  function playNote(note) {
+    synth.triggerAttackRelease(note, "8n")
+  }
+
+  navigator.requestMIDIAccess().then(access => {
+    const devices = access.inputs.values()
+    for (let device of devices) {
+      console.log(device);
+      device.onmidimessage = OnMidiMessage
+    }
+    console.log(access)
+  })
+
+  function OnMidiMessage(message) {
+    console.log(message);
+  }
+
+
+
+
+
   return (
     <div className="App">
 
@@ -15,15 +56,45 @@ function App() {
           <p>Patern:</p>
           <input defaultValue="1" min="1" type="number"></input>
         </div>
-        <select className="header_select">
-          <option selected>Home</option>
-          <option>Sequencer</option>
-          <option>Playlist</option>
-        </select>
+        <nav className="header_nav">
+          <ul className="header_nav-list">
+            <li title="Step Sequencer"><i className="fas fa-th-list"></i></li>
+            <li title="Tracker"><i className="fas fa-list-ol"></i></li>
+          </ul>
+        </nav>
       </header>
 
       <main>
-        {/* Route et composant */}
+        <div className="piano-container">
+          {
+            notes.map((item, index) => {
+              if (item.includes("#")) {
+                return (
+                  <button onClick={() => playNote(item)} key={index} value={item} className="black"></button>
+                )
+              }
+              else {
+                return (
+                  <button onClick={() => playNote(item)} key={index} value={item} className="white"></button>
+                )
+              }
+            })
+          }
+        </div>
+        <div className="roles-containter">
+          {notes.map(item => {
+            if(item.includes('#')){
+              return(
+                <div className="grid-item-black"></div>
+              )
+            }
+            else{
+              return(
+                <div className="grid-item-white"></div>
+              )
+            }
+          })}
+        </div>
       </main>
     </div>
   );
