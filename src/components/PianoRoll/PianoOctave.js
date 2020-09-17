a8e5a0a04f1f7a78975358a60ed8b640a6e5eaa7
Quentin Vandevorst <56817496+Survive95@users.noreply.github.com>
2020-09-17 09:00:42 +0200

    addGamePiano

--- src/components/PianoRoll/PianoOctave.js
+++ src/components/PianoRoll/PianoOctave.js
@@ -1,155 +1,174 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.scss';
 import * as Tone from 'tone'
-import { Loop } from 'tone';
+//import { Loop } from 'tone';
 
-export default function PianoRoll(props) {
+function PianoRoll(props) {
 
   const synth = new Tone.Synth().toDestination()
-
-  const note = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-  // note.reverse()
-  const notes = []
   let octave = props.octave
-  let nbColumn = 50
 
+  // const note = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  const note = [`C${octave}`, `C#${octave}`, `D${octave}`, `D#${octave}`, `E${octave}`, `F${octave}`, `F#${octave}`, `G${octave}`, `G#${octave}`, `A${octave}`, `A#${octave}`, `B${octave}`]
+  note.reverse()
+  // const notes = []
+  //let nbColumn = 50
 
-  
-  function loop(octave) {
-    for (let i = 0; i < 1 ; i++) {
-      note.map(item => {
-        notes.push(item + i + octave)
-      })
-    }
-    notes.reverse()
-  }
-  
-  loop(octave)
-  
+  const div = []
 
-  
-  var mouse_IsDown = false;
+  /* var mouse_IsDown = false;
   document.documentElement.addEventListener("mousedown", function () {
     mouse_IsDown = true;
   });
   document.documentElement.addEventListener("mouseup", function () {
     mouse_IsDown = false;
-  });
+  }); */
 
-  function playNote(note, e) {
+  const [savedNotes, setSavedNotes] = useState(localStorage.getItem('savedNote') ? JSON.parse(localStorage.getItem('savedNote')) : [])
+
+  const playNote = (note, e) =>  {
     synth.triggerAttackRelease(note, "8n")
     console.log(e.target)
-    if(e.target.classList.contains('test')){
+    if (e.target.classList.contains('test')) {
       e.target.classList.remove('test')
-      
-    }else{
+
+    } else {
       e.target.classList.add('test')
-     
+      console.log(note, e.currentTarget.getAttribute('data-id'))
+      let col = e.currentTarget.getAttribute('data-id')
+      let touche = note
+      setSavedNotes(old => [...old, {
+        col,
+        note: touche
+      }])
+
+      
     }
-    
-  
   }
 
-  navigator.requestMIDIAccess().then(access => {
-    const devices = access.inputs.values()
-    for (let device of devices) {
-      console.log(device);
-      device.onmidimessage = OnMidiMessage
-    }
-    console.log(access)
+
+  useEffect(() => {
+    let divs = document.querySelectorAll('.piano_grid div')
+
+    divs.forEach(element => {
+      element.classList.remove('test')
+    });
+
+    setTimeout(() => {
+      savedNotes.map(item => {
+        divs.forEach(element => {
+          if (element.getAttribute('data-id') === item.col && element.getAttribute('data-note') === item.note) {
+            element.classList.add('test')
+          }
+        });
+      })
+    }, 10);
+
+
+    localStorage.setItem('savedNote', JSON.stringify(savedNotes))
   })
 
-  function OnMidiMessage(message) {
-    console.log(message);
+
+
+
+  // navigator.requestMIDIAccess().then(access => {
+  //   const devices = access.inputs.values()
+  //   for (let device of devices) {
+  //     console.log(device);
+  //     device.onmidimessage = OnMidiMessage
+  //   }
+  //   console.log(access)
+  // })
+
+  // function OnMidiMessage(message) {
+  //   console.log(message);
+  // }
+
+
+  /* function loop() {
+    for (let i = 4; i < 5; i++) {
+      note.map(item => {
+        notes.push(item + i)
+      })
+    }
+    notes.reverse()
+  } */
+
+  function loopDiv() {
+    for (let d = 0; d < 20; d++) {
+      div.push(d)
+    }
   }
+
+  loopDiv()
+
+  // loop()
 
 
 
   return (
-      <div className="borad">
-        <div className="piano-container" id="keyboard">
-          
-          {
-            notes.map((item, index) => {
-              if (item.includes("#")) {
-                return (
-                  <div onClick={(e) => playNote(item,e)} key={index} value={item } className="black key"></div>
-                )
+    <div className="board">
+      <div className="clavier-container" id="keyboard">
+
+        {
+          note.map((item, index) => {
+            if (item.includes("#")) {
+              return (
+                <div onClick={() => synth.triggerAttackRelease(item, "8n")} key={index} value={item} className="black key"></div>
+              )
+            }
+            else {
+              if (item.includes('C')) {
+                if (item.includes('#')) {
+
+                }
+                else {
+                  return (
+                    <div onClick={() => synth.triggerAttackRelease(item, "8n")} key={index} value={item} className="white key">{item}</div>
+                  )
+                }
               }
               else {
                 return (
-                  <div onClick={(e) => playNote(item,e)} key={index} value={item } className="white key"></div>
+                  <div onClick={() => synth.triggerAttackRelease(item, "8n")} key={index} value={item} className="white key"></div>
                 )
               }
-            })
-          }
-        </div>
-        
-        <div className="roles-containter">
-          {notes.map((item, index) => {
-            if(item.includes('#')){
-              return(
-                <div  onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-black"></div>
-              )
             }
-            else{
-              return(
-                <div onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-white"></div>
-              )
-            }
-          })}
-        </div>
-          
-
-        <div className="roles-containter">
-          {notes.map((item, index) => {
-            if(item.includes('#')){
-              return(
-                <div  onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-black"></div>
-              )
-            }
-            else{
-              return(
-                <div onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-white"></div>
-              )
-            }
-          })}
-        </div>
-
-        <div className="roles-containter">
-          {notes.map((item, index) => {
-            if(item.includes('#')){
-              return(
-                <div  onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-black"></div>
-              )
-            }
-            else{
-              return(
-                <div onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-white"></div>
-              )
-            }
-          })}
-        </div>
-
-        <div className="roles-containter">
-          {notes.map((item, index) => {
-            if(item.includes('#')){
-              return(
-                <div  onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-black"></div>
-              )
-            }
-            else{
-              return(
-                <div onClick={(e) => playNote(item,e)} key={index} value={item } className="grid-item-white"></div>
-              )
-            }
-          })}
-        </div>
-        
+          })
+        }
       </div>
-     
+
+
+
+
+      <section className="piano-containter">
+        {note.map((touch, index) => {
+          return (
+            <section className={touch.includes('#') ? "piano_grid black" : "piano_grid white"}>
+              {div.map((item, index) => {
+                return <div
+                  data-note={touch}
+                  data-id={index}
+                  onClick={(e) => playNote(touch, e)}
+                  key={index}>{touch}</div>
+              })}
+              {touch.includes('#') ? <button onClick={(e) => playNote(touch, e)} key={index} className="piano_grid_note-black"></button> : <button onClick={(e) => playNote(touch, e)} key={index} className="piano_grid_note-white">{touch.includes('C') ? (touch.includes('#') ? "" : touch) : ""}</button>}
+            </section>
+          )
+        })}
+      </section>
+
+
+
+
+
+    </div>
+
 
   );
 }
+
+
+export default React.memo(PianoRoll)
 
 
--- src/components/PianoRoll/PianoRoll.js
+++ src/components/PianoRoll/PianoRoll.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.scss';
 import PinaoOctave from './PianoOctave'
 
 export default function PianoRoll() {
 
-  let octave = 4
+  //let octave = 4
+  /* let plusBtn = 4
+  let MoinsBtn = 4 */
+  
+  /* let [plusBtn, setplusBtn] = useState(4)
+  const [MoinsBtn, setMoinsBtn] = useState(4) */
 
+  const [octLength, setoctLength] = useState([5])
+
+  useEffect(() => {
+    // setoctLength(octLength.reverse())
+    //console.log(octLength);
+  })
+
+  const plusOcatve = () => {
+    //setOctLenght(old => )
+    setoctLength(old => [Math.max(...old) + 1, ...old])
+    
+    //console.log(octLength)
+  }
+  const moinsOctave = () => {
+    setoctLength(old => [...old, Math.min(...old) -1])
+    
+    //console.log(octLength)
+  }
+  
   return (
     <div className="Roll">
-      <PinaoOctave octave={octave+1} />
-      <PinaoOctave octave={octave}/>
+      <button onClick={() => plusOcatve()}>+</button>
+      <button onClick={() => moinsOctave()}>-</button>
       {/* <PinaoOctave octave={octave -1 }/> */}
+
+      {octLength.map((item, index) => {
+        return(
+          <PinaoOctave key={index} octave={item}/>
+        )
+      })}
+
     </div>
   );
 }
