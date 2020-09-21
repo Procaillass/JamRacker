f36b6ab047c67dc0414eabf447f0b2d98affd1fb
Mathieu <mathieu.slaedts@generis.be>
2020-09-21 09:11:55 +0200

    Datas in context

--- src/App.js
+++ src/App.js
@@ -5,11 +5,50 @@ import Instrument from './Components/Instrument/Instrument';
 /**Context for sequencer */
 import {BpmProvider} from './context/bpmContext';
 import {InstrumentProvider} from './context/instrumentContext';
+import {StepSeqProvider} from './context/stepSequencerContext';
 
 function App() {
+
+  /*
+   * -------------
+   * METHODS
+   * -------------
+   */
+  // REFACTORER EN HELPER OU HOOK CAR REPETE
+   const generateSteps = (stepsNum = 16) => Array.from({ length: stepsNum }, () => 0);
+
   /**State BPM */
   const [dataBpm, setDataBpm] = useState({bpm:100});
   const [dataInstrument, setDataInstrument] = useState("");
+  const [dataStepSeq, setdataStepSeq] = useState({
+
+    stepsNum: 16,
+    
+    notes: [
+    { name: "A0" }, { name: "B0" },
+    { name: "C1" }, { name: "D1" }, { name: "E1" }, { name: "F1" }, { name: "G1" }, { name: "A1" }, { name: "B1" },
+    { name: "C2" }, { name: "D2" }, { name: "E2" }, { name: "F2" }, { name: "G2" }, { name: "A2" }, { name: "B2" },
+    { name: "C3" }, { name: "D3" }, { name: "E3" }, { name: "F3" }, { name: "G3" }, { name: "A3" }, { name: "B3" },
+    { name: "C4" }, { name: "D4" }, { name: "E4" }, { name: "F4" }, { name: "G4" }, { name: "A4" }, { name: "B4" },
+    { name: "C5" }, { name: "D5" }, { name: "E5" }, { name: "F5" }, { name: "G5" }, { name: "A5" }, { name: "B5" },
+    { name: "C6" }, { name: "D6" }, { name: "E6" }, { name: "F6" }, { name: "G6" }, { name: "A6" }, { name: "B6" },
+    { name: "C7" }, { name: "D7" }, { name: "E7" }, { name: "F7" }, { name: "G7" }, { name: "A7" }, { name: "B7" },
+    { name: "C8" }
+  ],
+
+  tracks: [
+    {
+      name: "A0",
+      duration: 0,
+      steps: generateSteps(16)
+    },
+    {
+      name: "D7",
+      duration: 0,
+      steps: generateSteps(16)
+    },
+  ],
+});
 
   //Récupération du bpm lors du changement du range
   const handleBPM = (event) => {
@@ -21,6 +60,7 @@ function App() {
   return (
     <BpmProvider value={{dataBpm}}>
       <InstrumentProvider value={{dataInstrument, setDataInstrument}}>
+      <StepSeqProvider value={{dataStepSeq, setdataStepSeq}}>
       <div className="App">
         <header className="header">
           <button className="header_play-button"><i className="fas fa-play"></i></button>
@@ -49,6 +89,7 @@ function App() {
           </>
         </main>
         </div>
+        </StepSeqProvider>
         </InstrumentProvider>
     </BpmProvider>
   );
--- src/Components/StepSequencer/StepSequencer.jsx
+++ src/Components/StepSequencer/StepSequencer.jsx
@@ -8,6 +8,7 @@ import clap from "../../Assets/Sounds/clap.wav";
 import hat from "../../Assets/Sounds/hat.wav"; // voir les sons dans tone
 import BpmContext from "../../context/bpmContext";
 //import Play from "../../Components/Play/Play";
+import StepSeqContext from "../../context/stepSequencerContext";
 
 function StepSequencer() {
 
@@ -17,25 +18,18 @@ function StepSequencer() {
    * -------------
    */
   
+   // Il faut qu'il y ai la gestion des 1
    const generateSteps = (stepsNum = 16) => Array.from({ length: stepsNum }, () => 0);
 
   // ACTIVE / DEACTIVE STEPS
   const updateStep = (trackIdx, stepIdx, trackNote) => {
-    Tone.context.resume(); // deal with "The AudioContext was not allowed to start."
+    Tone.context.resume();
     const newTracks = [...tracks];
     newTracks[trackIdx].steps[stepIdx] = newTracks[trackIdx].steps[stepIdx] === 0 ? 1 : 0;
-    setTracks(newTracks);
-    sampler.triggerAttackRelease(trackNote, 0);
+    setdataStepSeq({...dataStepSeq, tracks: newTracks});
+    console.log(trackNote);
+    instru.triggerAttackRelease(trackNote, 0.2);
   };
-
-  const trackToSample = (tracks) => {
-
-    const newTracks = tracks
-      .filter((el) => el.type === "sampler")
-      .map((el) => [el.index, el.sound]);
-
-    return Object.fromEntries(newTracks);
-  }
 
   /*
    * -------------
@@ -45,6 +39,12 @@ function StepSequencer() {
   
   const bpmContext = useContext(BpmContext);
   const bpm = bpmContext.dataBpm.bpm;
+  // console.log("bpm", bpmContext);
+  
+  const {dataStepSeq, setdataStepSeq} = useContext(StepSeqContext);
+  const notes = dataStepSeq.notes;
+  const tracks = Array.isArray(dataStepSeq.tracks) && dataStepSeq.tracks.length ? dataStepSeq.tracks :  [];
+  const steps = dataStepSeq.stepsNum;
 
   /*
   * -------------
@@ -54,63 +54,7 @@ function StepSequencer() {
 
   const [playing, setPlaying] = useState(false);
   const [colIndex, setColIndex] = useState(0);
-  const [steps, setSteps] = useState(16);
-  const [sounds, setSounds] = useState([
-
-
-    //séquenceur {
-//     "timeSig": "4/4",
-//    "bpm": 120,
-//     "tracks": [
-//         [
-//             { "eventType": "note", "absTime": 0, "duration": 1, "midinote": 60 },
-//             { "eventType": "note", "absTime": 1, "duration": 1, "midinote": 64 },
-//             { "eventType": "note", "absTime": 3, "duration": 1, "midinote": 67 },
-//         ],
-//         [
-//             { "eventType": "note", "absTime": 0, "duration": 3, "midinote": 60 },
-//         ]
-//     ]
-// } -> contexte -> instrument <- choisir dans le newComposant
-
-    // note, durée
-
-    
-    {
-      index: "d0",
-      name: "clap",
-      sound: clap,
-      type: "sampler",
-      duration: 0,
-      steps: generateSteps(steps)
-    },
-    {
-      index: "e0",
-      name: "hat",
-      sound: hat,
-      type: "sampler",
-      duration: 0,
-      steps: generateSteps(steps)
-    },
-    {
-      index: "c0",
-      name: "kick",
-      sound: kick,
-      type: "sampler",
-      duration: 0,
-      steps: generateSteps(steps)
-    },
-    {
-      index: "b0",
-      name: "bassDrum",
-      sound: bassDrum,
-      type: "sampler",
-      duration: 0,
-      steps: generateSteps(steps)
-    }
-  ]);
-  const [tracks, setTracks] = useState([...sounds]);
-  const [sampler, setSampler] = useState(new Tone.Sampler(trackToSample(tracks)).toDestination());
+  const [instru, setInstru] = useState(new Tone.Synth().toDestination());
 
   /*
    * -------------
@@ -134,26 +78,27 @@ function StepSequencer() {
   const handleAddTrack = (ev) => {
     ev.preventDefault();
     if(ev.target.value !== "") {
-      const val = ev.target.value;
-      const newTrack = [...sounds].filter( (el) => el.name === val );
-      setTracks([...tracks, ...newTrack]);
-      sampler.add("c0", kick);
+      const note = ev.target.value;
+      const newTrack = { name: note, duration: 0, steps: generateSteps(steps) };
+      setdataStepSeq({...dataStepSeq, tracks: [...dataStepSeq.tracks, newTrack]});
     }
     ev.target.value = "";
   };
 
   const handleSteps = (ev) => {
     ev.preventDefault();
-    setSteps(stepsFld.current.value);
+    setdataStepSeq({...dataStepSeq, stepsNum: stepsFld.current.value});
   };
 
   const handleClose = (ev) => {
     ev.preventDefault();
-    alert("close");
+    //alert("close");
+    notes.map( (note, index) => tracks.map(el => el.name).includes(note.name) === true && console.log( 'ok' ));
   };
 
   const handleRemoveTrack = (id) => {
-    setTracks(tracks.filter((track) => track.index !== id));
+    setdataStepSeq({...dataStepSeq, tracks: tracks.filter((track) => track.index !== id)});
+    //setTracks();
   }
 
   /*
@@ -178,15 +123,10 @@ function StepSequencer() {
 
 
   //
-  /* useEffect(() => {
-    console.log("did mount");
-    console.log(sampler);
-  }, []) */
-
-
-  //
   useEffect(() => {
-    setTracks([...tracks].map(el => ({...el, steps: generateSteps(steps)})));
+    console.log("steps", steps);
+    const newTracks = [...tracks].map(el => ({...el, steps: generateSteps(steps)}));
+    setdataStepSeq({...dataStepSeq, tracks: newTracks});
   }, [steps])
 
 
@@ -197,7 +137,7 @@ function StepSequencer() {
       tracks.forEach((track, index) => {
         const step = track.steps[stepIndex.current];
         if (step === 1) {
-            sampler.triggerAttackRelease(tracks[index].index, track.duration);
+            instru.triggerAttackRelease(tracks[index].name, 0.2);
         }
       });
       setColIndex(stepIndex.current);
@@ -210,6 +150,9 @@ function StepSequencer() {
   * RENDER
   * -------------
   */
+ //console.log("context steps", dataStepSeq)
+
+console.log(dataStepSeq);
   return (
     
       <div className="box sequencer">
@@ -224,34 +167,35 @@ function StepSequencer() {
         <input className="box__stepsrange" type="range" min="4" max="64" step="4" ref={stepsFld} onChange={handleSteps} value={steps} />
 
         {tracks.map((track, trackIdx) => (
-          <div className="sequencer__row" key={trackIdx+"_"+track.name}>
-            <div className="sequencer__sound">
-              <button className="btn__removeTrack" onClick={() => {if(window.confirm('Are you sure you want to delete the track ?')){handleRemoveTrack(track.index)}}}>Remove sound</button>
-              <span>{track.name}</span>
-            </div>
-            <div className="sequencer__track">
-              {track.steps.map((step, stepIdx) => (
-                <div
-                  key={stepIdx}
-                  className={`sequencer__step ${step ? "sequencer__stepmarked" : ""} ${
-                    stepIdx === colIndex ? "sequencer__stepcol" : ""
-                  }`}
-                  onClick={() => updateStep(trackIdx, stepIdx, track.index)}
-                />
-              ))}
-            </div>
-          </div>
+    <div className="sequencer__row" key={trackIdx+"_"+track.name}>
+      <div className="sequencer__sound">
+        <button className="btn__removeTrack" onClick={() => {if(window.confirm('Are you sure you want to delete the track ?')){handleRemoveTrack(track.index)}}}>Remove sound</button>
+        <span>{track.name}</span>
+      </div>
+      <div className="sequencer__track">
+        {track.steps.map((step, stepIdx) => (
+          <div
+            key={stepIdx}
+            className={`sequencer__step ${step ? "sequencer__stepmarked" : ""} ${
+              stepIdx === colIndex ? "sequencer__stepcol" : ""
+            }`}
+            onClick={() => updateStep(trackIdx, stepIdx, track.name)}
+          />
         ))}
+      </div>
+    </div>
+  ))}
         
-        <div className="sequencer__controls">
+        {<div className="sequencer__controls">
           <select className="sequencer__addtrack" onChange={handleAddTrack}>
             <option value="">Add a track</option>
-            {sounds.map((sound, index) => (
-              <option key={index+'_'+sound} value={sound.name}>{sound.name}</option>
-            ))}
+            {notes.map((note, index) => 
+              tracks.map(el => el.name).includes(note.name) === false &&
+              <option key={index+'_'+note.name} value={note.name}>{note.name}</option>
+            )}
           </select>
           <button className="sequencer__play" onClick={handlePlaying}>{playing ? "stop" : "play"}</button>
-        </div>
+        </div>}
 
         </div>
 
