4aa42bb5050220a000e6ac1998091752bc1e1190
maud08 <maudinette08@outlook.com>
2020-09-25 09:22:24 +0200

    change title piano

--- src/components/PianoRoll/PianoOctave.js
+++ src/components/PianoRoll/PianoOctave.js
@@ -8,7 +8,7 @@ function PianoRoll(props) {
 
   /*
   * --------
-  * SYNTH
+  * SYNTH /
   * --------
   */
   
--- src/components/PianoRoll/PianoRoll.js
+++ src/components/PianoRoll/PianoRoll.js
@@ -7,7 +7,7 @@ export default function PianoRoll() {
 
   /*
   * --------
-  * CONTEXT
+  * CONTEXT /
   * --------
   */
 
